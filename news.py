d0020edc853dff60e8baebf236550e4a0f52e76f
vagrant <vagrant@vagrant.vm>
2019-01-08 10:35:32 +0000

    Apply PEP8 + update query results + python3

--- news.py
+++ news.py
@@ -7,18 +7,19 @@ from newsDB import get_articles, get_authors, get_days_with_error
 
 app = Flask(__name__)
 
-print "=====================\r\n"
-print "Most visited articles \r\n=====================\r\n"
+print("=====================")
+print("Most visited articles \n=====================")
 for row in get_articles():
-    print "\n" + str(row[0]) + ' - ' + str(row[1]) + ' views'
-print "\n\n\r\n=================================\r\n"
-print "Authors of Most visited articles \r\n"
-print "=================================\r\n"
+    print("\n" + str(row[0]) + ' - ' + str(row[1]) + ' views')
+print("\n\n=================================")
+print("Authors of Most visited articles")
+print("=================================")
 for row in get_authors():
-    print "\n" + str(row[0]) + ' - ' + str(row[1]) + ' views'
-print "\n\n\r\n==================================\r\n"
-print "Days with error ratio more than 1% \r\n"
-print "==================================\r\n"
+    print("\n" + str(row[0]) + ' - ' + str(row[1]) + ' views')
+print("\n\n==================================")
+print("Days with error ratio more than 1%")
+print("==================================")
 for row in get_days_with_error():
     ratio = ((row[3] / (row[1] + row[3])) * 100)
-    print "\n" + str(row[0]) + ' - ' + str(round(ratio, 2)) + '% errors'
+    print("\n" + str(row[0]) + ' - ' + str(round(ratio, 2)) + '% errors')
+print("\n\n====== Report completed ====== \n\n")
--- newsDB.py
+++ newsDB.py
@@ -1,4 +1,4 @@
-# Database code for the DB Forum, full solution!
+# DatabASe code for the DB Forum, full solution!
 
 import psycopg2
 import bleach
@@ -7,30 +7,55 @@ DBNAME = "news"
 
 
 def get_articles():
-    """Return all articles from the 'database', most visited first."""
+    """Return all articles from the 'databASe', most visited first."""
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("select articles.title as title, count(log.path) as views from log join articles on articles.slug = reverse(split_part(reverse(path), '/', 1)) group by title order by views DESC")
+    c.execute("""SELECT articles.title AS title, count(log.path) AS views
+                FROM log
+                JOIN articles
+                ON articles.slug = reverse(split_part(reverse(path), '/', 1))
+                GROUP BY title
+                ORDER BY views DESC limit 3""")
     articles = c.fetchall()
     db.close()
     return articles
 
 
 def get_authors():
-    """Return all authors from the 'database', most visited articles' author first."""
+    """Return authors with most visited articles."""
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("select authors.name as author, count(log.path) as views from log join articles on articles.slug = reverse(split_part(reverse(path), '/', 1)) join authors on articles.author = authors.id group by authors.name order by views DESC")
+    c.execute("""SELECT authors.name AS author, count(log.path) AS views
+                FROM log
+                JOIN articles
+                ON articles.slug = reverse(split_part(reverse(path), '/', 1))
+                JOIN authors ON articles.author = authors.id
+                GROUP BY authors.name
+                ORDER BY views DESC""")
     authors = c.fetchall()
     db.close()
     return authors
 
 
 def get_days_with_error():
-    """get all days where status code '404 NOT FOUNd' was more than 1% of the requests in that day."""
+    """Return days with error ratio > 1%."""
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("select * from (select time::timestamp::date as goodTime, count(status) as goodStatus from log where status = '200 OK' group by goodTime) as goodrequests, (select time::timestamp::date as badTime, count(status) as badStatus from log where status = '404 NOT FOUND' group by badTime) as badRequest where goodrequests.goodTime = badRequest.badTime and ((goodrequests.goodStatus +  badRequest.badStatus) * .01) < badRequest.badStatus")
+    c.execute("""SELECT *
+                FROM
+                (SELECT time::timestamp::date AS goodTime,
+                count(status) AS goodStatus
+                FROM log
+                WHERE status = '200 OK'
+                GROUP BY goodTime) AS goodrequests,
+                (SELECT time::timestamp::date AS badTime,
+                count(status) AS badStatus
+                FROM log
+                WHERE status = '404 NOT FOUND'
+                GROUP BY badTime)AS badRequest
+                WHERE goodrequests.goodTime = badRequest.badTime
+                and ((goodrequests.goodStatus +  badRequest.badStatus) * .01)
+                < badRequest.badStatus""")
     errors = c.fetchall()
     db.close()
     return errors
